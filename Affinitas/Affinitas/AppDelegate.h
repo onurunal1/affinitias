43c2b06abc79dc795d7a35a2af3d10d88e4e42a6
onur <onur@crowdee.de>
2015-11-10 20:13:38 +0100

    created new project template

---
+++ Affinitas/Affinitas/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Affinitas
+//
+//  Created by OnurMac on 11/10/15.
+//  Copyright © 2015 Onur Unal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Affinitas/Affinitas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Affinitas
+//
+//  Created by OnurMac on 11/10/15.
+//  Copyright © 2015 Onur Unal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
